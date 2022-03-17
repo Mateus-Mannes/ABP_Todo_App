5fa31471bbb8b5feebb2f599277e4d40ca3444be
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2022-03-17 20:28:02 -0300

    Create seed contributor

---
+++ src/TodoApp.Domain/ToDoAppDataSeederContributor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoApp.ToDoApp;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp.ToDoApp
+{
+    public class ToDoAppDataSeederContributor
+        : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<ToDoItem, Guid> _toDoItemRepository;
+
+        public ToDoAppDataSeederContributor(IRepository<ToDoItem, Guid> toDoItemRepository)
+        {
+            _toDoItemRepository = toDoItemRepository;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            if (await _toDoItemRepository.GetCountAsync() <= 0)
+            {
+                await _toDoItemRepository.InsertAsync(
+                    new ToDoItem
+                    {
+                        Text = "Levar o Lixo"
+                    },
+                    autoSave: true
+                );
+
+                await _toDoItemRepository.InsertAsync(
+                    new ToDoItem
+                    {
+                        Text = "Fazer trabalho"
+                    },
+                    autoSave: true
+                );
+            }
+        }
+    }
+}
